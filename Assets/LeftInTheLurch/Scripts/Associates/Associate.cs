e54a0dcea3e47ff91a3bac825a51a761a37eb0de
AndAgGit <afon.cyffrous@gmail.com>
2024-11-01 15:21:01 -0700

    Adding a debug mode
    enables debug.log statements

--- Assets/LeftInTheLurch/Scripts/Associates/Associate.cs
+++ Assets/LeftInTheLurch/Scripts/Associates/Associate.cs
@@ -3,19 +3,39 @@ using UnityEngine;
 
 public class Associate : MonoBehaviour
 {
+    public string title;
+
+    public bool debug;
+
     [Range(-10,10)]
     [Tooltip("DEBUGGING")]
-    private float standing;
+    public float standing;
 
     public float Affinity{
         get{ return Mathf.RoundToInt(standing); }
     }
 
     public void ChangeStanding(float delta){
+        if(debug){
+            Debug.Log($"adding {delta} to standing ({standing}): {standing + delta}");
+        }
+
         standing += delta;
     }
 
     public void SetStanding(float value){
+        if(debug){
+            Debug.Log($"setting standing to {value}");
+        }
+
         standing = value;
+    }
+
+    public void Log(){
+        if(debug){
+            Debug.Log($"{title}\nAffinity of {Affinity}\nStanding of {standing}");
+            return;
+        }
+        Debug.Log($"{title}\nAffinity of {Affinity}");
     }
 }
